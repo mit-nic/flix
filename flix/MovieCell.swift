c14d60421f2e95379ef7e1405c057d531e72edda
Nicolas Rodriguez <nhr@fb.com>
2017-06-21 15:48:47 -0700

    added MovieCell class and made cells functional with movie info

---
+++ flix/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Nicolas Rodriguez on 6/21/17.
+//  Copyright © 2017 Nicolas Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
